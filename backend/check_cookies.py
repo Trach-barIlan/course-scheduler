a788eca12dcd8577dfde0ad305e804a0f9cf71c6
Yonatan Trachtenberg <yonatan.trachtenberg@live.biu.ac.il>
2025-06-18 15:21:48 +0300

    Delete backend/check_cookies.py

--- backend/check_cookies.py
+++
@@ -1,8 +0,0 @@
-from itsdangerous import URLSafeTimedSerializer
-import os
-secret_key = os.environ.get('FLASK_SECRET_KEY', 'default-secret-key')  # Replace with your actual secret key
-cookie_value = 'eyJfcGVybWFuZW50Ijp0cnVlfQ.aFGc4Q.eKIJPUbbephDtVGkIhues_9iL2U'  # Replace with the value of the cookie
-
-serializer = URLSafeTimedSerializer(secret_key)
-session_data = serializer.loads(cookie_value)
-print(session_data)
